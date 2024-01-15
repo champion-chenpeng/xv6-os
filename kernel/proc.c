87a60c6281a952c192fec13d5afea0cf19d94f2b
Chen Peng <65433575+champion-chenpeng@users.noreply.github.com>
2024-01-15 12:09:50 -0800

    Done: USYSCALL

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,13 @@ found:
     return 0;
   }
 
+  // USYSCALL page
+  if ((p->usyscall = (struct usyscall *)kalloc()) == 0) {
+	freeproc(p);
+	release(&p->lock);
+	return 0;
+  }
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -139,6 +146,14 @@ found:
     release(&p->lock);
     return 0;
   }
+  
+  // put pid into USYSCALL page
+  memset(p->usyscall, 0, PGSIZE);
+  struct usyscall u;
+  u.pid = p->pid;
+  // store u at usyscall page
+  memmove(p->usyscall, &u, sizeof(u));
+  
 
   // Set up new context to start executing at forkret,
   // which returns to user space.
@@ -158,8 +173,13 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+
+  if (p->usyscall) 
+	kfree((void*)p->usyscall);
+
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -201,6 +221,13 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  // map usyscall, PTE_U for user mode
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
+	uvmunmap(pagetable, TRAMPOLINE, 1, 0); 
+	uvmunmap(pagetable, TRAPFRAME, 1, 0);
+	uvmfree(pagetable, 0);
+	return 0;
+  }
 
   return pagetable;
 }
@@ -212,6 +239,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
